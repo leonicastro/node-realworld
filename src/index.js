92b24c35e7efb7ff095b34b715e8d9b386303bde
Yuri Leoni de Castro <yuri.castro@sigma.com.br>
2019-08-15 13:31:17 -0400

    removido arquivo de config de rota e adicionado no index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './config/route-config';
+import path from 'path';
+import fs from 'fs';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(routes);
+
+/**
+ * Adiciona automaticamente as rotas
+ * */
+const directoryPath = path.join(__dirname, './app/routes/');
+fs.readdirSync(directoryPath)
+  .filter(file => !!file && file.indexOf('.') !== 0)
+  .forEach(file => {
+      let [routeName] = file.split('.');
+      app.use(`/${routeName === 'index' ? '' : routeName}`, require(path.resolve(directoryPath, file)));
+  });
 
 module.exports = app;
